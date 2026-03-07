3f880e3f519b37a624ae99da1daf3a5f3572b460
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2026-03-08 00:07:20 +0800

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="cr_text">Copyright © {{thisYear}} 心尘的工具集</div>
+    <div class="cr_text">Powered by <a href="https://wulvxinchen.cn/">吾律心尘</a></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      thisYear: new Date().getFullYear()
+    }},
+}
+</script>
+
+<style scoped>
+.cr_text {
+  text-align: center;
+  color: #888;
+  margin: 20px 0;
+  font-size: 14px;
+}
+</style>
