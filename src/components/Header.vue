a65628c90118f24baea89710ee40292f2159bf62
Lv-xinchen <63569234+drpasserby@users.noreply.github.com>
2026-03-14 07:44:22 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="header_container container">
     <h1>快速搜索文本</h1>
+    <div>
+      <a href="https://wulvxinchen.cn/">导航站</a>&nbsp;|
+      <a href="https://github.com/drpasserby/QuickSearchText/issues">反馈</a>
+      </div>
   </div>
 </template>
 
